33ebfd8cd879b9a1b65d4812e53c6c71201caa81
atharva417 <atharvamete12@gmail.com>
2021-08-10 23:54:00 +0530

    added body_rate control

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -1052,7 +1052,16 @@ void GCS_MAVLINK_Copter::handleMessage(const mavlink_message_t &msg)
         }
 
         // ensure type_mask specifies to use attitude and thrust
-        if ((packet.type_mask & ((1<<7)|(1<<6))) != 0) {
+        if ((packet.type_mask & (1<<6)) != 0) {
+            break;
+        }
+
+        // only one of these flags is true
+        bool use_body_rate_thrust = (packet.type_mask & (1<<7)) == 128;
+        bool use_yaw_rate = (packet.type_mask & ((1<<0) | (1<<1) | (1<<2))) == 3;
+        bool use_attitude = (packet.type_mask & ((1<<0) | (1<<1) | (1<<2))) == 7;
+        if (use_body_rate_thrust + use_yaw_rate + use_attitude > 1) {
+            ::printf("Invalid type_mask\n");
             break;
         }
 
@@ -1077,15 +1086,16 @@ void GCS_MAVLINK_Copter::handleMessage(const mavlink_message_t &msg)
             }
         }
 
-        // if the body_yaw_rate field is ignored, use the commanded yaw position
-        // otherwise use the commanded yaw rate
-        bool use_yaw_rate = false;
-        if ((packet.type_mask & (1<<2)) == 0) {
-            use_yaw_rate = true;
-        }
+        // // if the body_yaw_rate field is ignored, use the commanded yaw position
+        // // otherwise use the commanded yaw rate
+        // bool use_yaw_rate = false;
+        // if ((packet.type_mask & (1<<2)) == 0) {
+        //     use_yaw_rate = true;
+        // }
 
         copter.mode_guided.set_angle(Quaternion(packet.q[0],packet.q[1],packet.q[2],packet.q[3]),
                 climb_rate_or_thrust, use_yaw_rate, packet.body_yaw_rate, use_thrust);
+        copter.mode_guided.set_rate_bf_roll_pitch_yaw(packet.body_pitch_rate, packet.body_roll_rate, packet.body_yaw_rate, use_body_rate_thrust);
 
         break;
     }
